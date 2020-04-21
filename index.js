2f6c247fabaa1300bbe4fd7c676925cf2ce0d52d
Jui <jui20oct@gmail.com>
2020-04-21 12:41:54 +0530

    Comment on a scream

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions')
 
 const app = require('express')()
 
-const {getAllScreams, postOneScream} = require('./handlers/screams')
+const {getAllScreams, postOneScream, getScream, commentOnScream} = require('./handlers/screams')
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users')
 const FBAuth = require('./util/FBAuth')
 
@@ -11,6 +11,8 @@ app.get('/screams' , getAllScreams)
 //post a scream route - its okay to send only body(without sending handle in POST req), as Handle will come 
 //from the authorization
 app.post('/scream' , FBAuth , postOneScream)
+app.get('/scream/:screamId' , getScream)
+app.post('/scream/:screamId/comment' , FBAuth, commentOnScream)
 
 //users routes
 app.post('/signup', signup)
